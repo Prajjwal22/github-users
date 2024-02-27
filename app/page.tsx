b79cbf4871c99930c7be1d0e409b1ddbe77f47f3
prajjwal3 <prajjwal@khojdeal.com>
2024-02-27 13:50:22 +0530

    deboncing the api call

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const Home = () => {
   const fetchUsers = (query: string) => {
     setTimeout(async () => {
       try {
-        const token = "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";
+        const token = "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";
         setIsLoading(true);
         const response = await fetch(
           `https://api.github.com/search/users?q=${query}`,
